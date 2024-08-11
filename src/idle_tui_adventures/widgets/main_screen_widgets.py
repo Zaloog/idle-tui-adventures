ba0f8b8610a430aef0a841dc1063ef90173a8751
Zaloog <gramslars@gmail.com>
2024-08-11 14:00:22 +0200

    update tests

--- src/idle_tui_adventures/widgets/main_screen_widgets.py
+++ src/idle_tui_adventures/widgets/main_screen_widgets.py
@@ -61,7 +61,7 @@ class CharacterProgressbar(ProgressBar):
 
     def _on_compose(self, event: Compose) -> Coroutine[Any, Any, None]:
         self.timer = self.set_interval(
-            1, self.make_progress, pause=True, name="ProgressbarTimer"
+            1, self.make_progress, pause=False, name="ProgressbarTimer"
         )
         return super()._on_compose(event)
 
